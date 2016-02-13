d64c392ca20b0436a37f5d9c964f766898f4b491
IvanVP <elsant@nextmail.ru>
2016-02-13 14:04:53 +0300

    Adds gems Slim, Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  # get 'pages/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  
 
-  # You can have the root of your site routed with "root"
+
   root 'pages#index'
 
   # Example of regular route:
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get index" do
+
+  test "should get root - index page" do
     get :index
     assert_response :success
+    assert_not_empty response.body
+    assert_match /LADIES\*DRESS\*GENTS/, response.body 
+    assert_select 'p', "YOUR STYLIST AWAITS"
   end
 
 end
