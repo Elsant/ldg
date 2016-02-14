4e9d642843cd0df663ce4e0534fcb3482809ccfc
IvanVP <elsant@nextmail.ru>
2016-02-14 23:45:47 +0300

    Adds additional fields to User

--- db/migrate/20160213111725_devise_create_users.rb
+++ db/migrate/20160213111725_devise_create_users.rb
@@ -1,8 +1,21 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string  :first_name,         null: false
+      t.string  :last_name,          null: false
+      t.string  :email,              null: false, default: ""
+      # t.string  :slug,               null: false
+      t.date    :birthdate 
+      t.string  :phone,              null: false
+      t.string  :zipcode,            null: false
+      t.text    :description
+      t.boolean :enabled,            default: true
+      t.integer :style_id
+      t.integer :size_id
+      t.string  :market_source # How Did You Hear about us?
+
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      # t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -34,6 +47,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
 
+    add_index :users, :first_name
+    add_index :users, :last_name
+    add_index :users, :zipcode
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
