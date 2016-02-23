eb6ad67f85c77cd4f2ab19e9ce9b7b9e96dff739
IvanVP <elsant@nextmail.ru>
2016-02-24 01:26:11 +0300

    Corrects after signup path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -48,8 +48,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_up_path_for(resource)
-    root_path
+  # def after_sign_up_path_for(resource)
+  #   login_path
+  # end
+
+  def after_inactive_sign_up_path_for(resource)
+    login_path
   end
 
   def after_update_path_for(resource)
